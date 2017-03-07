2279caf1e8860892c6ef208f90916d52da4e4512
square123 <1006039518@qq.com>
2017-03-07 18:45:48 +0800

    昨天的程序有点小问题今天已经修正。发现电脑计算速度很快

--- csvCmp/csvCmp/源.cpp
+++ csvCmp/csvCmp/源.cpp
@@ -46,8 +46,12 @@ int main()
 	Csv csv1(filename1);//元素显示测试：
 	char filename2[] = "C://Users//Administrator//Desktop//RSSI优选//wifiTest//probe2.csv"; //文件名
 	Csv csv2(filename2);//元素显示测试：
+	char filename3[] = "C://Users//Administrator//Desktop//RSSI优选//wifiTest//probeSysc.csv"; //文件名
+	Csv csv3(filename3);//元素显示测试：
 	vector <vector <string>> result;    //存储表格结构的二维vector
-
+	string buffer1;
+	string buffer2;
+	string buffer3;
 	for(auto it1 = csv1.table.begin(); it1 < csv1.table.end()-1; ++it1)
 	{    
 		auto it2 = it1 -> begin();
@@ -62,31 +66,75 @@ int main()
 			const char* sTchar2=sTemp2.c_str();//得到第2个数据
 			string mac2=itt2[1];
 			string rssi2=itt2[2];
+
 			if((charToTimeInt(sTchar2)-charToTimeInt(sTchar1))>5)
 				break;//大于时间跳出
 			if(timeCompare(sTchar2,sTchar1,1))
 			{
 				if(mac1==mac2)
 				{
-					vector <string> rsows;
-					rsows.push_back(sTemp1);
-					rsows.push_back(mac1);
-					rsows.push_back(rssi1);
-					rsows.push_back(rssi2);
-					result.push_back(rsows);
+					if((buffer1==sTemp1)&&(buffer2==mac1)&&(buffer3==rssi1))
+					{
+					
+					}
+					else{
+						vector <string> rsows;
+						buffer1=sTemp1;
+						buffer2=mac1;
+						buffer3=rssi1;
+						rsows.push_back(sTemp1);
+						rsows.push_back(mac1);
+						rsows.push_back(rssi1);
+						rsows.push_back(rssi2);
+						result.push_back(rsows);
+					}
 				}
 			}
 		}
 	}	
+	cout<<"result!"<<endl;
+	//int count=0;
+	////比较部分
+	//for(auto resIt1=result.begin();resIt1 < result.end(); ++resIt1)
+	//{	
+	//	bool outflag=1;
+	//	auto resIt2=resIt1->begin();
+	//	string time1=resIt2[0];
+	//	string mac1=resIt2[1];
+	//	string rssi11=resIt2[2];
+	//	string rssi12=resIt2[3];
+	//	for (auto csv3It1=csv3.table.begin();csv3It1 < csv3.table.end()-1; ++csv3It1)
+	//	{
+	//		auto csv3It2=csv3It1->begin();
+	//		string time2=csv3It2[0];
+	//		string mac2=csv3It2[1];
+	//		string rssi21=csv3It2[2];
+	//		string rssi22=csv3It2[3];
+	//		if ((time1==time2)&&(mac1==mac2)&&(rssi11==rssi21)&&(rssi12==rssi22))
+	//		{
+	//			outflag=0;
+	//			break;
+	//		}
+	//	}
+	//	if (outflag)
+	//	{
+	//		cout<<resIt2[0]<<" "<<resIt2[1]<<" "<<resIt2[2]<<" "<<resIt2[3]<<endl;
+	//		++count;
+	//	}
+	//}
+	//cout<<count<<endl;
+	cout<<"finish"<<endl;
 	//ofstream outfile;  
 	//outfile.open("C://Users//Administrator//Desktop//RSSI优选//wifiTest//computerSysn.csv",ios::out);  
-	////输出文件
+	////标准流方式输出文件
 	//for(auto t1 = result.begin(); t1 != result.end(); ++t1)
 	//{
 	//	auto t2 = t1 -> begin();
 	//	outfile<<t2[0]+","+t2[1]+","+t2[2]+","+t2[3];
 	//}
 	//outfile.close(); 
+
+	//C风格输出文件方式
 	FILE *fpSysc;
 	fpSysc=fopen("C://Users//Administrator//Desktop//RSSI优选//wifiTest//computerSysn.csv","a+");
 	for(auto t1 = result.begin(); t1 != result.end(); ++t1)
@@ -119,6 +167,7 @@ int main()
 	}
 	cout<<"finish"<<endl;
 	fclose(fpSysc);
+
 	getchar();
 	return 0;
 }
