198d9a51c15f31bdc2e51abb40c94673a70e4c6a
square123 <1006039518@qq.com>
2017-04-10 19:41:19 +0800

    修改了一部分类参数

--- 002探针数据/wifiTest/WifiProbe.h
+++ 002探针数据/wifiTest/WifiProbe.h
@@ -9,6 +9,7 @@
 #include<time.h>//加入时间
 #include <fstream>//希望输入时间
 #include <cmath>
+#include <vector>
 #include "MncatsWifi.h"
 #pragma comment(lib,"WS2_32.lib")
 class Probe
@@ -19,9 +20,13 @@ public:
 	~Probe();
 	//主要函数
 	void InitProbe();//初始化探针数据
-	void probeProcess();//探针处理数据部分
-	void rssiPurify(char time[14],mncatsWifi &Probedata,FILE *f,int index);//进行同一时间的RSSI优选
-	void probeSysc(FILE *f);//处理多探针同步的函数
+	void probeProcess();//探针处理数据部分 
+	void rssiIntegrate(char time[14],mncatsWifi &Probedata,int index);//作为新探针的整合程序
+	void rssiMissGet();//用于找出数据为空的集合
+	void rssiMissNot();//用于填补数据为空的集合
+	void rssiForMac();//用于找出对应Mac的RSSI序列集合
+	void rssiForMacAnalyticed();//用于特定序列的变换 暂定，或许以后可以合并
+
 	//其他子函数
 	char MaxRssi(char rssi1,char rssi2);//返回较大的RSSI值，且该值不能能等于0
 	char NormRssi(char a[],int ccc);//利用高斯滤波来选取rssi
@@ -33,73 +38,58 @@ public:
 private:	
 
 	//全局常量定义部分
+	   //探针部分
 	static const int sameTimeMacNum=30; //表示同一时刻内探针默认最多存储的数据类型数
 	static const int ProbeNum=3; //表示一共有几个探针返回数据
+	static const int THD=-70;
+	static const int buffNum=5; //这个参数很关键，要调的合适一些，调的过大过小都会出现出现数据丢失的问题，目前为4的时候数据较好。
+	   //socket通信部分
 	static const int SERVER_PORT=2222 ;
 	static const int BUFFER_SIZE=2048 ;
-	static const int THD=-90;
-	static const int buffNum=5; //这个参数很关键，要调的合适一些，调的过大过小都会出现出现数据丢失的问题，目前为4的时候数据较好。
-	static const int baseIndex=0;
-	static const int rssiCapacity=30;//这个数是存同一数据同一时间的RSSI的容量
+	//static const int baseIndex=0;
 
+	//中间变量部分
+		//socket通信用
 	WSADATA wsd;//WSADATA 变量
 	SOCKET s;//嵌套字
 	sockaddr_in servAddr;//服务器地址
 	sockaddr_in clientAddr; //客户端地址
 
-	//文件存储部分
-	FILE *fp1,*fp2,*fp3,*fpSysc;
+		//文件存储部分
+	FILE *fpGet,*fpNot;
 
-	//探针优选部分
-	unsigned char zeroMac[6];
+		//探针rssiIntegrate()优选部分
+	unsigned char zeroMac[6];//提前定义好0变量
 	char zeroTimestamp[14];
 	char zeroRssi;
-	char timeTemp[ProbeNum][14];//用来区分时间变化的变量
-	char timeBuffer[ProbeNum][14];//时间的寄存器，用来存储上上次的时间
-	int indexForPure[ProbeNum];
-	int indexForPureBuf[ProbeNum];//作为上一变量的寄存器
-	bool flag[ProbeNum];
-	struct rssiTemp
+	struct rssiTemp //定义处理数据的结构体
 	{
+		char Timestamp[14];
 		unsigned char selMumac[6];
 		char maxRssi;
-		//char normRssi;//辅助
-	}sel[ProbeNum][sameTimeMacNum];
-	rssiTemp selBuffer[ProbeNum][sameTimeMacNum];//是上一变量的寄存器
+	}sel[ProbeNum][60][sameTimeMacNum];
+	int rssiTempIndex[ProbeNum][60];//存储探针记录的索引
 
-	//同步部分
-	struct syscProbed
+		//探针rssiIntegrate()优选部分
+	struct rssiMiss //定义补全后数据的结构体
 	{
 		char Timestamp[14];
-		unsigned char selMumac[6];
-		char Rssi[ProbeNum];
-		//char NRssi[ProbeNum];//辅助
-	}syscResult[sameTimeMacNum];//用来输出同步后的结果
+		unsigned char Mumac[6];
+		char Rssi[ProbeNum];//记录探针的数据
+	}seled[60][sameTimeMacNum];//用来存储填补好rssi数据的结构体
+	int rssiMissIndex[60];//存储探针记录的索引
+	time_t processGetIndex,processNotIndex;
 
-	
+		//时间校准部分
 	time_t syscTimeBuff[ProbeNum];
 	time_t detProbeTime[ProbeNum];//用来存储同步时间差异的
-	int storeIndex;//存储数据的索引
-	int processIndex;//处理同步的索引
-	struct syscProbe	//同步多探针的结构体	配合baseIndex的要完成同步的数据，这里直接改成60，按秒取余存取，虽然浪费了空间但节约了时间，提高了鲁棒性
-	{
-		char Timestamp[14];
-		unsigned char selMumac[6];
-		char Rssi;
-		//char NRssi;//辅助
-	}syscStr[60][ProbeNum][sameTimeMacNum];//三维结构体数组，要寄存的时间、探针数量、每秒钟存储的数据
-	syscProbe syscStrForIndex[buffNum][sameTimeMacNum];//baseIndex使用 负责存储的数据，只是要完成延时的功能
-	syscProbe zeroSysc;//定义存储全零为syscProbe格式
-	char rssiData[ProbeNum][sameTimeMacNum][rssiCapacity];//存rssi的for normrssi
-	int rssiIndex[ProbeNum][sameTimeMacNum];//用于推进RSSI的索引
-	int storeIndexBuffer;//同步函数的标志位
 
 	//其他子程序
 	double myErf(double x);//erf函数
 	double myNormCdf(double x);//正态分布函数
-	time_t selectSysPrbTime(syscProbe sysc[sameTimeMacNum]);//输出结构体时间的函数
-	void reduceSyscProbe(syscProbe sysc[sameTimeMacNum],syscProbe sysced[sameTimeMacNum]);//将结构体数据紧凑
-	void AllreduceSyscProbe(syscProbe Allsysc[buffNum][ProbeNum][sameTimeMacNum],syscProbe Allsysced[buffNum][ProbeNum][sameTimeMacNum]);//将整个结构体数组缩减
+	//time_t selectSysPrbTime(syscProbe sysc[sameTimeMacNum]);//输出结构体时间的函数
+	//void reduceSyscProbe(syscProbe sysc[sameTimeMacNum],syscProbe sysced[sameTimeMacNum]);//将结构体数据紧凑
+	//void AllreduceSyscProbe(syscProbe Allsysc[buffNum][ProbeNum][sameTimeMacNum],syscProbe Allsysced[buffNum][ProbeNum][sameTimeMacNum]);//将整个结构体数组缩减
 };
 
 #endif
