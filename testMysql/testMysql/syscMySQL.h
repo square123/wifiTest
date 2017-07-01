750f9b438eb8b76eb625a9ebb43b6cddbd5a9891
square123 <1006039518@qq.com>
2017-07-01 22:08:27 +0800

    更新了部分函数

--- testMysql/testMysql/syscMySQL.h
+++ testMysql/testMysql/syscMySQL.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <algorithm>
 using namespace std;  
 //关于读写问题，考虑分时间段处理，现在先不考虑，可以将数据先存一部分到本地，让每个数据都能在存储后再处理。
 
@@ -22,19 +23,20 @@ private:
 	struct rssiData  //主要是有关数的结构需要转换下，便于后续去比较
 	{
 		string macName;
-		string timeName;
+		string time;
 		int rssi[4];
 	};
 	struct camData   //主要是有关数的结构需要转换下，便于后续去比较
 	{
 		string name;
-		string timeName;
+		string time;
 		int dis[4];//应该最后要修改类型 将所有的double都乘以1000转换成int，或者以后再转换接口
 	};
 	struct matchResult //算法最后的输出结果用 在匹配算法中，不需要考虑其他的，只考虑数就行，结构采用之前的就行，定义该结构就是方便存储
 	{
 		string macName;//mac地址
 		int score;//分数
+		int num; //需要两次更新分数
 	};
 public:
 	syscMySQL();
@@ -51,8 +53,9 @@ public:
 
 	//匹配算法 输出的应该是一个向量  vector<matchResult> 包括Mac地址和分数 包含很多元素 输出算法应该有排序的成分在里面
 	//最后的输入应该是一个人物的数据和多个探针的数据，在函数中应该还需要处理，最后的输出应该是排序后的输出
-	//算法一定要取交集不然会出错
-	void matchProcess(vector<camData> &cam,vector<rssiData> &rssi,vector<matchResult> &res);
+	void matchProcess(const char *srcCamTable,const char *srcRssiTable,const char *camName,const char *timeBegin,const char *timeEnd,vector<matchResult> &res);//单独排序的操作
+	void singleMatch(vector<camData> &src,vector<rssiData> &dst,matchResult &result);//算法一定要取交集不然会出错
+	void renewResult(vector<matchResult> &src);
 private:
 
 	//选取一个时间段的函数 涉及到表的操作 这些应该都是先在mysql上的表进行操作，最后才读取数据
@@ -70,8 +73,9 @@ private:
 	string charToString(char x);//char 转 string 函数
 	string intToString(int x);//int 转 string 函数
 	string doubleToString(double x);//double 转string函数
+	int stringToInt(string x);//string 转int函数
 	//算法中最后的排序函数
-	
+	bool resultSort(const matchResult &r1,const matchResult &r2);
 };
 
 //存数据的时候应该单独写个函数，不用很复杂
@@ -337,6 +341,19 @@ string syscMySQL::doubleToString(double x)//char 转 string 函数
 	return stream.str();
 }
 
+int syscMySQL::stringToInt(string x)
+{
+	int temp;
+	stringstream stream;
+	stream<<x;
+	stream>>temp;
+	if (!stream.good()) 
+	{ 
+		cout<<"error transform strToInt";
+	}
+	return temp;
+}
+
 void syscMySQL::selTimeBetween(const char *srcTable,const char *dstTable,const char *timeBegin,const char *timeEnd)//尽量减少mysql表的操作，减少在程序里的操作
 {
 	string sqlstr="create table ";
@@ -395,10 +412,13 @@ void syscMySQL::rssiDataGet(const char *srcTable,const string name,vector<rssiDa
 		row = mysql_fetch_row(result);  
 		while (NULL != row) {  
 			rssiData ttt;
-			ttt.
-			 
-				dst.push_back(row[i]);
-			 
+			ttt.timeName=row[0];
+			ttt.macName=row[1];
+			ttt.rssi[0]=stringToInt(row[2]);
+			ttt.rssi[1]=stringToInt(row[3]);
+			ttt.rssi[2]=stringToInt(row[4]);
+			ttt.rssi[3]=stringToInt(row[5]);
+			dst.push_back(ttt);
 			row = mysql_fetch_row(result);  
 		}  
 		mysql_free_result(result);  
@@ -413,10 +433,145 @@ void syscMySQL::rssiDataGet(const char *srcTable,const string name,vector<rssiDa
 
 void syscMySQL::camDataGet(const char *srcTable,const string name,vector<syscMySQL::camData> &dst)//读取cam数据的函数
 {
+	string sqlstr="SELECT * FROM ";
+	sqlstr+=srcTable;
+	sqlstr+=" WHERE name='";
+	sqlstr+=name;  
+	sqlstr+="' order by name asc;";
+	MYSQL_RES *result = NULL; 
+	if (0 == mysql_query(&mydata, sqlstr.c_str())) 
+	{
+		result = mysql_store_result(&mydata);  
+		MYSQL_ROW row = NULL;  
+		row = mysql_fetch_row(result);  
+		while (NULL != row) {  
+			camData ttt;
+			ttt.timeName=row[0];
+			ttt.name=row[1];
+			ttt.dis[0]=stringToInt(row[2]);
+			ttt.dis[1]=stringToInt(row[3]);
+			ttt.dis[2]=stringToInt(row[4]);
+			ttt.dis[3]=stringToInt(row[5]);
+			dst.push_back(ttt);
+			row = mysql_fetch_row(result);  
+		}  
+		mysql_free_result(result);  
+	}
+	else
+	{
+		cout << "mysql_query() select data failed" << endl;  
+		mysql_free_result(result);  
+		mysql_close(&mydata); 
+	}  
+}
+
+void syscMySQL::singleMatch(vector<camData> &src,vector<rssiData> &dst,matchResult &result)
+{
 
 }
 
-void syscMySQL::matchProcess(vector<camData> &cam,vector<rssiData> &rssi,vector<syscMySQL::matchResult> &res)
+void syscMySQL::matchProcess(const char *srcCamTable,const char *srcRssiTable,const char *camName,const char *timeBegin,const char *timeEnd,vector<matchResult> &res)
 {
+	vector<camData> cam;
+	//构建数据表
+	selTimeBetween(srcCamTable,"tempCam",timeBegin,timeEnd);
+	selTimeBetween(srcRssiTable,"tempRssi",timeBegin,timeEnd);
+	//从摄像头表中获取camName的信息
+	camDataGet("tempCam",camName,cam);
+	//获取rssi中存在的mac地址
+	vector<string> rssiName;
+	uniqueMac("tempRssi","macAddress",rssiName);
+	for (auto i:rssiName)
+	{
+		vector<rssiData> rssi;
+		matchResult resSingle;
+		rssiDataGet("tempRssi",i,rssi);
+		singleMatch(cam,rssi,resSingle);
+		res.push_back(resSingle);
+	}
+	//对匹配的结果分数进行更新
+	renewResult(res);
+	//排序
+	sort(res.begin(),res.end(),resultSort);
+	dropTable("tempCam");
+	dropTable("tempRssi");
+}
 
+bool syscMySQL::resultSort(const matchResult &r1,const matchResult &r2)
+{
+	return r1.score<r2.score;
+}
+
+//因为在记录过程中已经将信息优化为一秒，所以一秒对应多个的问题
+void syscMySQL::singleMatch(vector<camData> &src1,vector<rssiData> &src2,matchResult &result)//匹配过程的关键，一定要取两个数据的交集
+{
+	//默认在存储时，元素已经是拍好序的   所以其匹配速度为O(n)
+	//默认camData是一直有值的，不存在某时刻有rssi值没有cam值，则交集就是src2本身
+	//如果实际中，camData没有数据，则数据可能需要先取交集。
+	vector<camData> tempCam;//camData 需要有一个存储的过程，所以不能改变其变量
+	auto it1=src1.begin();
+	auto it2=src2.begin();
+	while (it1!=src1.end()&&it2!=src2.end())//这么写是为了以防万一，万一默认数据这里不对也需要修改
+	{
+		if (it1->time < it2->time)
+		{
+			it1++;
+		}else if (it2->time < it1->time)
+		{
+			it2++;
+		}else
+		{
+			tempCam.push_back(*it1);
+			it1++;
+			it2++;
+		}
+	}
+	//需要将对应位置存储起来，然后构成一个表格，此时表格大小是已知的，所以可以根据src存在的值进行对应
+	//将表格横着放，每行是一个探针所探测到的信息 
+	//觉得用动态数组不够好，而且存在很多的冗余，直接采用vector进行计算
+// 	int *tableCam = new int [tempCam.size()*4]();
+// 	int *tableRssi = new int [tempCam.size()*4]();
+	vector<int> rssi4;
+	vector<int> cam4;
+	for (int ind=0;ind<tempCam.size();++ind) //通过上述操作，此时已经一一对应
+	{
+		//按照对应存在的位置将cam也存进来
+		if (src2[ind].rssi[0]!=0)
+		{
+			rssi4[0].push_back(src2[ind].rssi[0]);
+			tableCam[ind*4+0]=src1[ind].dis[0];
+		}
+		if (src2[ind].rssi[1]!=0)
+		{
+			tableRssi[ind*4+1]=src2[ind].rssi[1];
+			tableCam[ind*4+1]=src1[ind].dis[1];
+		}
+		if (src2[ind].rssi[2]!=0)
+		{
+			tableRssi[ind*4+2]=src2[ind].rssi[2];
+			tableCam[ind*4+2]=src1[ind].dis[2];
+		}
+		if (src2[ind].rssi[3]!=0)
+		{
+			tableRssi[ind*4+3]=src2[ind].rssi[3];
+			tableCam[ind*4+3]=src1[ind].dis[3];
+		}	
+	}
+	//计算表格中数据的特征值,找出非零的特征
+
+
+	//进行比较
+
+	//最后输出分数和特征个数
+
+	//释放内存
+// 	delete[] tableCam;
+// 	delete[] tableRssi;
+// 	tableCam=NULL;
+// 	tableRssi=NULL;
+}
+
+void syscMySQL::renewResult(vector<matchResult> &src)//重新根据数量再对分数进行修改
+{
+	//sigmoid函数
 }
